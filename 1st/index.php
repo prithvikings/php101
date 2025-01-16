0dbdbf1563ac3e1b8c350cf950187b3fc2132aaf
prithvikings <prithvi07raj07@gmail.com>
2025-01-16 14:48:53 +0530

    This is the begining of php

---
+++ 1st/index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "My first program in PHP";
+for($i=0;$i<10;$i++)
+{
+    if($i%2==0){
+        echo "\nEven number: $i\n";
+    }else{
+        echo "Odd number: $i\n";
+    }
+}
+
+?>
