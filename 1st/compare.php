92b1118f274050d0ea23376f8d8264016ac506f3
prithvikings <prithvi07raj07@gmail.com>
2025-01-17 12:38:50 +0530

    Update comparison logic and add define.php with constants

--- 1st/compare.php
+++ 1st/compare.php
@@ -1,10 +1,10 @@
 <?php 
-$num1=28;
+$num1=7;
 if($num1 >10){
     echo "Number is greater than 10";
 }else if($num1==10){
     echo "Number is equal to 10";
 }else{
-    echo "Number is less than 10";
+    echo "Number is lesser than 10";
 }
 ?>
---
+++ 1st/define.php
@@ -0,0 +1,6 @@
+<?php
+define("PI",3.14);
+define("GREETING","Welcome to the world of PHP");
+echo "The value of PI is: ".PI."\n";
+echo GREETING;
+?>
