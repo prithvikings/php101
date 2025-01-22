83db0c20bc492f4868e396f1479a34384c2b262a
prithvikings <prithvi07raj07@gmail.com>
2025-01-23 00:05:38 +0530

    Add voting system login page with HTML structure and Tailwind CSS styling

--- Project/Voting/index.php
+++ Project/Voting/index.php
@@ -1,17 +1,3 @@
 <?php
-
-
-
+include 'index.html';
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
-    hi
-</body>
-</html>
