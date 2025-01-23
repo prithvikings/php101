532fed831991c385dbf115492a070af6ba53a8ca
prithvikings <prithvi07raj07@gmail.com>
2025-01-23 23:07:50 +0530

    Add debugging output to login.php for variable inspection during login process

--- Project/Voting/action/login.php
+++ Project/Voting/action/login.php
@@ -7,16 +7,18 @@ $phone = $_POST['phonenumber'];
 $password = $_POST['password'];
 $std = $_POST['std'];
 
-$sql="Select * from users where username='$username'
-and phone='$phone'
-and password='$password'
-and std='$std'";
-$result=mysqli_query($conn,$sql);
+// Debugging with var_dump
+var_dump($username, $phone, $password, $std);
 
-if(mysqli_num_rows($result)>0){
+// Stop execution temporarily to inspect the output
+die();
+
+$sql = "SELECT * FROM users WHERE username='$username' AND phone='$phone' AND password='$password' AND std='$std'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
     echo '<script>alert("Login Successful"); window.location.href="../Pages/vote.php";</script>';
-}else{
+} else {
     echo '<script>alert("Login Failed"); window.location.href="../index.php";</script>';
 }
-
 ?>
