72d65c7eb04d1ddfe3375461a4fc384a9e7a3c3f
prithvikings <prithvi07raj07@gmail.com>
2025-01-24 19:52:27 +0530

    maded the signup and sign in page workingg properly

--- Project/Voting/action/login.php
+++ Project/Voting/action/login.php
@@ -7,17 +7,20 @@ $phone = $_POST['phonenumber'];
 $password = $_POST['password'];
 $std = $_POST['std'];
 
-// Debugging with var_dump
-var_dump($username, $phone, $password, $std);
-
-// Stop execution temporarily to inspect the output
-die();
-
-$sql = "SELECT * FROM users WHERE username='$username' AND phone='$phone' AND password='$password' AND std='$std'";
+// Query to fetch user data
+$sql = "SELECT * FROM users WHERE username = '$username' AND phone = '$phone' AND std = '$std'";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-    echo '<script>alert("Login Successful"); window.location.href="../Pages/vote.php";</script>';
+    // Fetch the user's data
+    $row = mysqli_fetch_assoc($result);
+    
+    // Verify the hashed password
+    if (password_verify($password, $row['password'])) {
+        echo '<script>alert("Login Successful"); window.location.href="../Pages/vote.php";</script>';
+    } else {
+        echo '<script>alert("Invalid Password"); window.location.href="../index.php";</script>';
+    }
 } else {
     echo '<script>alert("Login Failed"); window.location.href="../index.php";</script>';
 }
